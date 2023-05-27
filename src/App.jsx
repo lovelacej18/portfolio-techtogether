6f2602108cb0ff5fb28292e0389d3569744eb9ec
J L <lovelacej18@gmail.com>
2023-05-27 14:42:57 -0400

    Revised App.jsx to update personal info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "J Lovelace",
+  title: "Undergraduate Computer Science Student & Research Assistant",
+  email: "lovelacej18@gmail.com",
+  gitHub: "lovelacej18",
+  instagram: "lovelacej18",
+  linkedIn: "jlovela",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
