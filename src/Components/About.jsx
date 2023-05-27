c104418525685fa629b45515996ee686bb3ded4a
J L <lovelacej18@gmail.com>
2023-05-27 14:48:22 -0400

    Updated About.jsx components to include personalized professional info

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an Undergraduate Computer Science student studying at Stevens Institute of Technology.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Computer vision",
+  "Data analysis",
+  "Research", 
+  "Technical writing"
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "There is a way out of every box, a solution to every puzzle; it's just a matter of finding it.";
 
 const About = () => {
   return (
